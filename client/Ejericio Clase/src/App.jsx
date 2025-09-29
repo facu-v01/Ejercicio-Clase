ab5471029dd8e27de07aeedbdfe17df9bcba91e4
facu_v <facundoandreassi@gmail.com>
2025-09-29 09:26:06 -0300

    Producto

--- client/Ejericio Clase/src/App.jsx
+++ client/Ejericio Clase/src/App.jsx
@@ -1,18 +1,31 @@
 import './App.css'
 import UserCard from './components/UserCard.jsx'
 import CustomButton from './components/CustomButton';
+import { Producto } from './components/Producto.jsx';
 
 
 function App() {
   return (
     <>
     <UserCard
-    name="Juan Pérez"
-    email="juanperez@example.com"
-    age={28}
-    isOnline={true}
+      name="Juan Pérez"
+      email="juanperez@example.com"
+      age={28}
+      isOnline={true}
     />
 
+    <Producto 
+      name="Mesa de Pino"
+      price={25000}
+      category="Muebles de Comedor"
+    />
+
+    <Producto 
+      name="Sillón"
+      price={30000}
+      discount={25}
+      category="Muebles de Comedor"
+    />
       
     </>
   )
